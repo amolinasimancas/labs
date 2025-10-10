d09a7769d2cc1ca74de1170dedd3f31b7d6f676a
amolinasimancas <antoniomolina282@gmail.com>
2025-10-10 13:43:41 +0000

    Update 10.10.2025

---
+++ ruta_chatGPT/calculadora_estadistica/main.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    print("Bienvenido a la Calculadora Estadística!\n    Selecciona la opción deseada y presiona enter:")
+    eleccion = input("    1. Carga vía archivo datos.csv\n    2. Carga manual\n>>> ")
