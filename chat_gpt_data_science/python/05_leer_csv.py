c7985ef4cc310e74d1a318ab566a58f04ba94196
amolinasimancas <antoniomolina282@gmail.com>
2025-10-25 22:13:12 +0000

    Erased code from other sources than web pages

--- chat_gpt_data_science/python/05_leer_csv.py
+++
@@ -1,4 +0,0 @@
-def leer_csv_a_lista(ruta):
-    return
-
-leer_csv_a_lista("datos.csv") # [(1.0, 2.0), (3.0, 4.0), (5.0, 6.0)]
--- chat_gpt_data_science/python/06_interpolacion_lineal.py
+++
@@ -1,4 +0,0 @@
-def interpolar(x0, y0, x1, y1, x):
-    pass
-
-interpolar(0, 0, 10, 100, 5)  # 50.0
