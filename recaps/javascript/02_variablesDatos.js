80616d876a23de69d41980c84fe31aaf8bded1fb
antoniomolinasimancas <antoniomolinasimancas@gmail.com>
2025-11-03 18:49:34 +0000

    Update 03.11.2025

---
+++ recaps/javascript/02_variablesDatos.js
@@ -0,0 +1,23 @@
+// Comentario en JavaScript
+
+let contador = 0;
+const PI = 3.14159;
+
+// Tipos de datos
+
+let nombre = 'Antonio';
+let edad = 38;
+let esCasado = true;
+let sinValor = null;
+let sinDefinir;
+let frutas = ['manzana', 'pera', 'banana', 'naranja'];
+let legajo = Symbol('1275');
+let numeroGrande = 900719925470991n;
+let auto = {
+    marca: 'Fiat',
+    modelo: 'Fastback',
+};
+function saludar(nombre){
+    console.log('Hola ' + nombre);
+};
+saludar('Antonio');
---
+++ recaps/javascript/03_strings.js
@@ -0,0 +1,12 @@
+let nombre = 'Antonio';
+let apellido = 'Molina';
+
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log(`Hola ${nombre} ${apellido}`);
+
+console.log(nombre.length);
+console.log(nombre.toUpperCase());
+console.log(nombre.toLowerCase());
+
+let saludo = `Hola ${nombre} ${apellido}`;
+console.log(saludo.substring(0,4));
