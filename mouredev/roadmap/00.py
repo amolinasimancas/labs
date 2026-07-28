0f0e5eac795e3a4d9c677a79230a763d18c75106
sidersaurio <sidersaurio@gmail.com>
2026-07-28 15:28:05 +0000

    Updated repo

---
+++ mouredev/roadmap/00.py
@@ -0,0 +1,19 @@
+# https://www.python.org/
+
+# Comentario de una sola línea
+'''
+Comentario multilínea
+Línea 1
+Línea 2
+Línea 3
+'''
+
+name = "Antonio"
+surname = "The Squirrel"
+
+print(f"Hello, I'm {name}, also known as {surname}!")
+
+age = 39
+isMarried = True
+
+print("Hello Python!")
